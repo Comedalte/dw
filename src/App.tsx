7d40ca0560b22e2b0266e7b3095b361be7409957
Henrik Andersson <comedalte@hotmail.com>
2021-03-12 23:50:09 +0100

    lint fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from "react";
 import Header from "./Header";
 import Showcase from "./Showcase";
@@ -6,25 +5,27 @@ import "./App.css";
 import { Product, ProductDto } from "../contracts/product";
 
 function App() {
-	const [products, setProducts] = useState<Product[]>([]);
-	console.log("build");
-	useEffect(() => {
-		const fetchData = async () => {
-			const response = await fetch("http://localhost:3333/products", { mode: "cors" });
-			const productDto: ProductDto = await response.json();
-			setProducts(productDto.products);
-		}
-		fetchData();
-	}, []);
+  const [products, setProducts] = useState<Product[]>([]);
+  console.log("build");
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3333/products", {
+        mode: "cors",
+      });
+      const productDto: ProductDto = await response.json();
+      setProducts(productDto.products);
+    };
+    fetchData();
+  }, []);
 
-	return (
-		<div className="container">
-			<Header />
-			<section>
-				<Showcase products={products} />
-			</section>
-		</div>
-	);
+  return (
+    <div className="container">
+      <Header />
+      <section>
+        <Showcase products={products} />
+      </section>
+    </div>
+  );
 }
 
 export default App;
